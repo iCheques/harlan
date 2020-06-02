34f7484ca11c482b71a35bce6085fe968081e806
Davi Almeida <davistrife@live.com>
2020-06-02 10:51:24 -0300

    Correção Async

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -11,7 +11,7 @@ module.exports = controller => {
     // const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos/index.js').fail(failAlert));
     const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-veiculos@latest/index.js').fail(failAlert));
     const graficosAnaliticosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-graficos-analiticos@1.0.6/index.js').fail(failAlert));
-    /*const consultaSimplesCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-consulta-simples@1.0.1/index.js').fail(failAlert));*/
+    const consultaSimplesCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-consulta-simples@1.0.2/index.js').fail(failAlert));
     const followCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-follow-document@1.3.13/index.js').fail(failAlert));
     const statuspageCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-statuspage@latest/index.js').fail(failAlert));
     const pdfMonitoramento = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-monitoramento-pdf@latest/index.js').fail(failAlert));
@@ -28,7 +28,7 @@ module.exports = controller => {
         relatorioAnalitico();
         componenteVeiculosCall();
         graficosAnaliticosCall();
-        //consultaSimplesCall();
+        consultaSimplesCall();
     }).fail(() => {
         callback();
         failAlert();
