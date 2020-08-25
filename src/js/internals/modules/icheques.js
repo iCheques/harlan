b02557d0378cfd58d646eb2bb7ec0a6e6c93680b
Davi Almeida <davistrife@live.com>
2020-08-25 08:38:21 -0300

    Fix Refin

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -7,7 +7,7 @@ module.exports = controller => {
         paragraph: 'Verifique se o endereço cdn.jsdelivr.net é liberado na sua rede interna.',
     });
 
-    const refinCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-refin@1.0.46/index.js').fail(failAlert));
+    const refinCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-refin@1.0.47/index.js').fail(failAlert));
     // const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos/index.js').fail(failAlert));
     const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-veiculos@1.1.23/index.js').fail(failAlert));
     const graficosAnaliticosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-graficos-analiticos@1.0.9/index.js').fail(failAlert));
