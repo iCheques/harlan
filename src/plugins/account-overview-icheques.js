a659403a154640261c7df9d2d82c515e230a76d1
Davi Almeida <davistrife@live.com>
2020-09-25 12:25:46 -0300

    Fix

--- src/plugins/account-overview-icheques.js
+++ src/plugins/account-overview-icheques.js
@@ -163,7 +163,7 @@ harlan.addPlugin(controller => {
                         controller.call('myIChequesAccountOverview::filter', username, report, callback, closeable);
                     });
                     callback(report);
-                    const historicoConsultas = oneTime(() => $.getScript( "https://cdn.jsdelivr.net/npm/harlan-credithub-historico-consultas@1.0.9/index.js" )
+                    const historicoConsultas = oneTime(() => $.getScript( "https://cdn.jsdelivr.net/npm/harlan-credithub-historico-consultas@1.0.10/index.js" )
                     .fail(function( jqxhr, settings, exception ) {
                         console.log("Triggered ajaxError handler.");
                     }));
