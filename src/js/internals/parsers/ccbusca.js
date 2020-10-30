cb2d7680974c76b8ea9ccee60dd995c61169e16d
Davi Almeida <davistrife@live.com>
2020-10-30 14:29:53 -0300

    Fix

--- src/js/internals/parsers/ccbusca.js
+++ src/js/internals/parsers/ccbusca.js
@@ -622,6 +622,13 @@ module.exports = controller => {
             result.addItem('Capital Social', numeral(capitalSocial.text().replace('.', ',')).format('$0,0.00'), 'capitalSocial');
         }
 
+        if (doc) {
+            controller.trigger('ccbusca::parser', {
+                result,
+                doc
+            });
+        }
+
         /*setAddress(result, jdocument);
         setAddress(result, jdocument, true);*/
         //setSocio(result, jdocument);
@@ -633,13 +640,11 @@ module.exports = controller => {
         setQSA(result, jdocument);
         setSociety(result, jdocument);
 
-        if (doc) {
-            controller.trigger('ccbusca::parser', {
-                result,
-                doc
-            });
-        }
-        
+        controller.trigger('processoJuridicoParser', {
+            result,
+            doc
+        });
+
         return result.element();
     };
 
