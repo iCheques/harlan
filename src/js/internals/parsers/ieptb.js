749141486597328460dc1a36123d55fce212484c
Davi Almeida <davistrife@live.com>
2020-12-02 16:19:46 -0300

    Fix protestos

--- src/js/internals/parsers/ieptb.js
+++ src/js/internals/parsers/ieptb.js
@@ -89,18 +89,27 @@ module.exports = controller => {
 
         let data = dates.length ? moment.max(dates).format('DD/MM/YYYY') : 'Não Informado';
         valorTotalDeProtestos = valorTotalDeProtestos > 0 ? numeral(valorTotalDeProtestos).format('$0,0.00') : 'Não Informado';
-        result.element().append(fieldsCreator.addSeparator('Resumo de Protestos'));
+        /*result.element().append(fieldsCreator.addSeparator('Resumo de Protestos'));
         fieldsCreator.addItem('Total de Protestos', totalDeRegistros);
         fieldsCreator.addItem('Última Ocorrência ', data);
         fieldsCreator.addItem('Valor Total de Protestos', valorTotalDeProtestos);
 
         result.element().append(fieldsCreator.element());
-        fieldsCreator.resetFields();
+        fieldsCreator.resetFields();*/
+
+        const separatorProtestosEmCartorio = result.addSeparator('Protestos em Cartório');
+
+        if(parseInt(totalDeRegistros)) separatorProtestosEmCartorio.css('margin-top', '40px').css('background', 'url(images/textures/brilliant.png),linear-gradient(180deg,#f70808,#fff 160%)');
 
         _.each(jdocument.find('BPQL > body > consulta > conteudo > cartorio'), element => {
-            result.addSeparator('Protestos em Cartório',
+            const cartorioSeparator = result.addSeparator('Protestos em Cartório',
                 $('nome', element).text(),
                 $('endereco', element).text()).css('margin-top', '40px').css('background', 'url(images/textures/brilliant.png),linear-gradient(180deg,#f70808,#fff 160%)');
+            cartorioSeparator.hide();
+            cartorioSeparator.find('.container').remove();
+
+            const nomeCartorio = result.addItem('Nome do Cartório', $('protestos', element).text());
+            result.addItem('Endereço do Cartório', $('endereco', element).text());
 
             /*result.addItem('Protestos', $('protestos', element).text()).addClass('center');
             result.addItem('Telefone', $('telefone', element).text());
@@ -110,12 +119,15 @@ module.exports = controller => {
 
             $('protesto', element).each((i, v) => protestosDoCartorio.push(parseFloat($('valor', v).text())));
 
+            result.addItem('Protestos', $('protestos', element).text());
+
             if (protestosDoCartorio.length) {
                 const valorTotal = protestosDoCartorio.reduce((a, b) => a + b);
                 result.addItem('Valor Total no Cartório', numeral(valorTotal).format('$0,0.00'));
+            } else {
+                return;
             }
 
-            result.addItem('Protestos', $('protestos', element).text());
             let cidade = $('cidade', element).text();
             if (cidade) result.addItem('Cidade', cidade);
 
@@ -143,7 +155,36 @@ module.exports = controller => {
                 if (data && !/^\s*$/.test(data)) result.addItem('Data do protesto', moment(data, ['YYYY-MM-DD', 'DD-MM-YYYY']).format('DD/MM/YYYY'));
                 if (valor && !/^\s*$/.test(valor)) result.addItem('Valor do protesto', numeral(valor.replace('.', ',')).format('$0,0.00'), 'valor');*/
             });
-            result.element().append(fieldsCreator.element());
+
+            fieldsCreator.element().css({
+                borderBottom: '2px solid #2196F3'
+            }).hide();
+
+            const collapseBtn = $('<i>').addClass('fa fa-chevron-circle-down').css({ marginTop: '0.4em', cursor: 'pointer' });
+            const botaoMostrarProcessos = $('<div>').css({
+                backgroundColor: 'rgb(251, 65, 36)',
+                color: '#fff',
+                textAlign: 'center',
+                padding: '5px',
+                fontWeight: 'bold',
+                marginBottom: '20px',
+            }).text('Mostrar Protestos').append($('<br>'))
+            .append(collapseBtn);
+
+            const elementFields = fieldsCreator.element();
+
+            result.element().append(elementFields);
+            botaoMostrarProcessos.insertAfter(nomeCartorio.parents().eq(1));
+            collapseBtn.on('click', (ev) => {
+                ev.preventDefault();
+                if (collapseBtn.hasClass('fa-chevron-circle-down')) {
+                    collapseBtn.removeClass('fa-chevron-circle-down').addClass('fa-chevron-circle-up');
+                    elementFields.show(500);
+                } else {
+                    collapseBtn.removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+                    elementFields.hide(500);
+                }
+            });
             fieldsCreator.resetFields();
         });
 
