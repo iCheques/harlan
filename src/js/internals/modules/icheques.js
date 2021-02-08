7d39d5c5dbdf782ad30a58c90e19aef64e33b260
Davi Almeida <davistrife@live.com>
2021-02-08 17:21:05 -0300

    Fix

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -11,7 +11,7 @@ module.exports = controller => {
     // const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-icheques-veiculos/index.js').fail(failAlert));
     const veiculosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-veiculos@1.1.32/index.js').fail(failAlert));
 
-    const graficosAnaliticosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-graficos-analiticos@1.0.31/index.js').fail(failAlert));
+    const graficosAnaliticosCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-graficos-analiticos@1.0.33/index.js').fail(failAlert));
 
     const consultaSimplesCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-consulta-simples@1.0.7/index.js').fail(failAlert));
 
@@ -41,7 +41,7 @@ module.exports = controller => {
 
     const notifyCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-notify@1.0.1/index.js').fail(failAlert));
 
-    controller.registerCall('harlanVersion', () => '1.0.38');
+    controller.registerCall('harlanVersion', () => '1.0.40');
 
     controller.call('SafariError');
     controller.call('LocationError');
