69abaf5534e08d0748c05cfa790a0c4eb3daa2db
Davi Almeida <davistrife@live.com>
2021-02-24 15:31:36 -0300

    Fix version

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -41,7 +41,7 @@ module.exports = controller => {
 
     const notifyCall = oneTime(() => $.getScript('https://cdn.jsdelivr.net/npm/harlan-credithub-notify@1.0.10/index.js').fail(failAlert));
 
-    controller.registerCall('harlanVersion', () => '1.0.54');
+    controller.registerCall('harlanVersion', () => '1.0.55');
 
     controller.call('SafariError');
     controller.call('LocationError');
